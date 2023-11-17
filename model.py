6a642163b9fa450237138fd694d642b1a70fea09
Noctibus <stanislas.debet@gmail.com>
2023-11-17 12:14:49 +0100

    Regex & test result

--- model.py
+++ model.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from read_data import read_csv, preprocess_csv, preprocess_text
 
+df = read_csv()
+df = preprocess_csv(df)
+df = preprocess_text(df)
 
-
+print(df["Words list"][10])
+print(df["Concatenated Words"][10])
