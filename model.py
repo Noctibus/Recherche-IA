fcc822795ff634e97028470a3cba870e2701d408
Noctibus <stanislas.debet@gmail.com>
2023-11-17 16:33:29 +0100

    cosine similiarity & model

--- model.py
+++ model.py
@@ -1,9 +1,20 @@
 import pandas as pd
 from read_data import read_csv, preprocess_csv, preprocess_text
+from sentence_transformers import SentenceTransformer
 
 df = read_csv()
 df = preprocess_csv(df)
 df = preprocess_text(df)
 
-print(df["Words list"][10])
-print(df["Concatenated Words"][10])
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+def get_embeddings(df:pd.DataFrame):
+    embedings = model.encode(df["Concatenated Words"].tolist())
+    df_result = pd.DataFrame()
+    df_result["Marque"] = df["Marque"]
+    df_result["Embeddings"] = embedings.tolist()
+    return df_result
+
+df_result = get_embeddings(df)
+df_result.to_csv("embeddings.csv", sep=';', encoding='UTF-8', index=False)
