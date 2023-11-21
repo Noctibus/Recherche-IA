68a6f5c06247412bed3783483ff455b76e4ac787
Noctibus <stanislas.debet@gmail.com>
2023-11-21 10:38:04 +0100

    matrix

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -20,37 +20,43 @@ def get_embeddings_dataframe():
     return df_result
 
 
-def vector_from_patent(patent):
-    # return the vector from the patent
-    vector = 0
-    n = len(patent)
-    for i in range(n):
-        vector += patent[i]
-    return vector / n
+def get_vector_from_brand(df:pd.DataFrame, name:str):
+    # look for the column embeddings in the row "name" from the column "Marque" in the dataframe "df" and return it, if patent exists
+    if type(name) != str:
+        # print("Patent name must be a string. Here is the type: {} and the content: {}".format(type(name), name))
+        return None
+    try:
+        ret = df.loc[df["Marque"] == name.lower(), "Embeddings"].values[0]
+    except IndexError:
+        print("Patent not found")
+        ret = None
 
 
-def cosine_similarity(v1, v2):
+def cosine_similarity(brand_a:str, brand_b:str, df:pd.DataFrame=None):
+    if df is None:
+        df = get_embeddings_dataframe()
+    vector_a = get_vector_from_brand(df, brand_a)
+    vector_b = get_vector_from_brand(df, brand_b)
+    # Check if patent exists
+    if vector_a is None or vector_b is None:
+        return None
     # compute the cosine similarity between two vectors
-    numerator = np.dot(v1, v2)
+    numerator = np.dot(vector_a, vector_b)
     den_a, den_b = 0, 0
-    for i in range(len(v1)):
-        den_a += v1[i] ** 2
-        den_b += v2[i] ** 2
+    for i in range(len(vector_a)):
+        den_a += vector_a[i] ** 2
+        den_b += vector_b[i] ** 2
     denominator = sqrt((den_a ** 0.5) * (den_b ** 0.5))
     return numerator / denominator
 
 
-def get_vector_from_patent(df, name):
-    # return only the embeddings, not the name neither the index
-    return df.loc[df['Marque'] == name, 'Embeddings'].iloc[0]
+# df = get_embeddings_dataframe()
 
-df = get_embeddings_dataframe()
+patent_a = "reenix"
+patent_b = "monjour"
 
-vector_a = get_vector_from_patent(df, "KANYAR")
-vector_b = get_vector_from_patent(df, "KANYAR")
+# vector_a = get_vector_from_patent(df, "kaNyar")
+# vector_b = get_vector_from_patent(df, "kanYar")
 
 
-print("Cosine similarity: ", cosine_similarity(vector_a, vector_b))
-
-# export the whole column Marque from the dataframe and save it into a file
-df["Marque"].to_csv("marques.csv", sep=';', encoding='UTF-8', index=False)
+print("Cosine similarity: ", cosine_similarity(patent_a, patent_b))
