f6b1c2e87f5101904de9c8409755a99463c18505
FannyGobbo <fanny.gobbo00@gmail.com>
2024-01-07 14:54:19 +0100

    add test samplings for creating false datas

--- test_sampling.py
+++ test_sampling.py
@@ -1,23 +1,32 @@
 import pandas as pd
+import nltk
+from nltk.corpus import wordnet
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 
-# retourne le dataframe original mis à jour, et le dataframe crée 
-def extract_real_datas (df:pd.DataFrame):
+# retourne le dataframe original mis à jour, et les dataframes crées
+def extract_sample_datas (df:pd.DataFrame):
     SEED = 2023
-    nb_sample = 200
+    nb_sample = 100
 
-    # Extraction de nb_sample lignes
-    df_test = df.sample(n=nb_sample, random_state=SEED)
+    # Extraction de nb_sample lignes pour les vraies données
+    df_real = df.sample(n=nb_sample, random_state=SEED)
 
     # Les supprime du dataframe
-    df = df.drop(df_test.index)
+    df = df.drop(df_real.index)
     
-    return df ,df_test
+    # Extraction de nb_sample lignes pour les données à falsifier
+    df_false = df.sample(n = nb_sample, random_state=SEED)
+    
+    
+    
+    return df, df_real, df_false
 
 
 
 
-# Donne une liste des antonymes pour chaque entrée
+# Donne une liste des synonymes pour chaque entrée
 def get_synonyms(words):
     synonyms = set()
     for word in words:
@@ -26,18 +35,39 @@ def get_synonyms(words):
                 synonyms.add(lemma.name())
     return list(synonyms)
 
-# Donne une liste des contraires pour chaque entrée
-def get_antonyms_french(words):
-    antonyms = set()
-    for word in words:
-        for syn in wordnet.synsets(word, lang='fra'):
-            for lemma in syn.lemmas('fra'):
-                for antonym in lemma.antonyms():
-                    antonyms.add(antonym.name())
-    return list(antonyms)
 
 
-def create_false_datas (df:pd.DataFrame):
-    df_test = df.sample(n=nb_sample, random_state=SEED)
-    # TODO : vérifier liste synonymes / antonymes si rien existe => substitution / sinon choix de 1 des deux si existe both
+
+def create_false_datas (df:pd.DataFrame, df_false:pd.DataFrame):
+    nb_real = 100
+    SEED = 2023
+    placeholder_word = 'cacahuette'
+    placeholder_date = '31/12/2023'
+
+    df_real['Status'] = 'original'
+    df_false['Status'] = 'plagiat'
+
+    for index, brand in df_false['Marque'].items():
+        modified_brand = ''
+        for word in brand.split():
+            syn_list = get_synonyms(word)
+            if syn_list:
+                modified_brand += syn_list[0] + ' '
+            else:
+                modified_brand += word + ' '
+        modified_brand = modified_brand.strip()
+        if modified_brand == brand:
+            words = modified_brand.split()
+            words[0] = placeholder_word
+            modified_brand = ' '.join(words)
+
+        df_false.at[index, 'Marque'] = modified_brand
+        df_false.at[index, 'Date de dépôt/enregistrement'] = placeholder_date
+    return pd.concat([df_real, df_false])
+    
+    
+    
+    
+    
+    
 
